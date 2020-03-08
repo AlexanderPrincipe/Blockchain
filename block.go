91e0de8277386444d5684ba41a2947944554ed3b
Alexander <principetolentinoa@hotmail.com>
2020-03-08 00:10:48 -0500

    blockchain

--- block.go
+++ block.go
@@ -1,5 +1,11 @@
 package main
-import "strconv"
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+import "fmt"
 
 type Block struct {
 	Timestamp int64
@@ -9,12 +15,27 @@ type Block struct {
 }
 
 func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, base:10))
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headres)
+	hash := sha256.Sum256(headers)
 	b.Hash = hash[:]
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block  {
-	
+	block := &Block{
+		Timestamp: time.Now().Unix(),
+		Data:	   []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Hash:      []byte{},
+	}
+	block.SetHash()
+	return block
+}
+
+func main()  {
+	b := NewBlock("Probando nuestro bloque", []byte("65df2s4"))
+	fmt.Println("Timestamp", b.Timestamp)
+	fmt.Println("Data:", string(b.Data))
+	fmt.Println("PrevBlockHash:", string(b.PrevBlockHash))
+	fmt.Println("Hash:", string(b.Hash))
 }
